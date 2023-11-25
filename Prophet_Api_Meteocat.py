2be2b2522e041bb3df68a38bc630a01707a8d2ce
carlotamiriam <carlota_miriam@hotmail.com>
2023-11-25 16:30:59 +0100

    Imports
    Imports

---
+++ Prophet_Api_Meteocat.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from pandas import json_normalize
+import numpy as np
+import requests
+from datetime import date, timedelta, datetime
+import time
+import os
+import json
+from pathlib import Path
+import glob
+import seaborn as sns
+from prophet import Prophet
+from matplotlib import pyplot as plt
+
